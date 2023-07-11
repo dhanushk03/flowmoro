f5be6e5f560a66c76e7d1a3e411af52b2281d7dc
Dhanush Kalangi <dhanushk1303@gmail.com>
2023-07-10 20:09:26 -0400

    Initial attempt at timer

--- timerapp/src/App.js
+++ timerapp/src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [hours, setHours] = useState("");
+  const [minutes, setMinutes] = useState("");
+  const [seconds, setSeconds] = useState("");
+  const [startTime, setStartTime] = useState(0);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [intervalId, setIntervalId] = useState(0);
+  //const [paused, setPaused] = useState(true);
+
+  function padzeroes(num){
+    return String(num).length == 1? "0" + num : String(num);
+  }
+  
+  //setTimeout(updateTime, 1000);
+  
+  function updateTime(){
+    setElapsedTime(Date.now() - startTime);
+
+    var secs = Math.floor((elapsedTime / 1000) % 60);
+    var mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    var hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+
+    //update time display
+    setSeconds(padzeroes(secs));
+    setMinutes(padzeroes(mins));
+    setHours(padzeroes(hrs));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{hours}:{minutes}:{seconds}</h1>
     </div>
   );
 }
