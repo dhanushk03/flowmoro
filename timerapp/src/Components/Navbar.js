a2ac3d2e888bc877cbd4efb203f4a8940c586aa0
Dhanush Kalangi <dhanushk1303@gmail.com>
2023-07-27 13:08:02 -0400

    Restyled navbar by removing left and right container divs

--- timerapp/src/Components/Navbar.js
+++ timerapp/src/Components/Navbar.js
@@ -4,16 +4,11 @@ import { Link } from "react-router-dom";
 const Navbar = (props) => {
     return (
         <div className="navbar">
-            <div className="navbar-left">
-                <li className="flmdlogo"><img src="./flowmodoro_logo.jpg" alt="logo" width="30px" height="30px"/></li>
-                <Link to="/" className="navbar-item" id="homepage"> Flowmodoro </Link>
-            </div>
-            <div className="navbar-right">
-                <Link to="/timer" className="navbar-item" id="timer"> Timer </Link>
-                <Link to="/todolist" className="navbar-item" id="todolistnav"> To-do List </Link>
-                <Link to="/studylog" className="navbar-item" id="studylognav"> Study Log </Link>
-                <Link to="/news" className="navbar-item" id="news"> News </Link>
-            </div>
+                <Link to="/" className="navbar-item navbar-left" activeClassName="active" activeStyle={{ color: "blue" }} id="homepage"> <li className="flmdlogo"><img src="./flowmodoro_logo.jpg" alt="logo" width="30px" height="30px"/></li>&#160;&#160;&#160;Flowmodoro</Link>
+                <Link to="/timer" className="navbar-item navbar-right" activeClassName="active" activeStyle={{ color: "blue" }} id="timer"> Timer </Link>
+                <Link to="/todolist" className="navbar-item navbar-right" activeClassName="active" activeStyle={{ color: "blue" }} id="todolistnav"> To-do List </Link>
+                <Link to="/studylog" className="navbar-item navbar-right" activeClassName="active" activeStyle={{ color: "blue" }} id="studylognav"> Study Log </Link>
+                <Link to="/news" className="navbar-item navbar-right" activeClassName="active" activeStyle={{ color: "blue" }} id="news"> News </Link>
         </div>
     );
 }
